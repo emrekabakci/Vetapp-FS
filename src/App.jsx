176f75bfdca51e708327668b29a868b36cc7c608
emrekabakci <221238040@ogrenci.karatay.edu.tr>
2024-06-09 20:55:50 +0300

    Route redirects were made.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Appointments from './pages/Appointments';
+import Customers from './pages/Customers';
+import Animals from './pages/Animal';
+import Doctors from './pages/Doctors'
+import Vaccine from './pages/Vaccine';
+import Reports from './pages/Reports';
 
 const App = () => {
   return (
@@ -11,6 +16,11 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/appointments" element={<Appointments />} />
+        <Route path="/customers" element={<Customers />} />
+        <Route path="/animals" element={<Animals />} />
+        <Route path="/doctors" element={<Doctors />} />
+        <Route path="/vaccine" element={<Vaccine />} />
+        <Route path="/report" element={<Reports />} />
       </Routes>
     </Router>
   );
